06b5dcfcebcb111d02e7af4938e16dedc5589f18
georgeh04 <69112643+georgeh04@users.noreply.github.com>
2024-06-16 23:12:37 +0100

    commit

--- lib/media_session.dart
+++ lib/media_session.dart
@@ -1,8 +1,11 @@
-
 import 'media_session_platform_interface.dart';
 
 class MediaSession {
   Future<String?> getPlatformVersion() {
     return MediaSessionPlatform.instance.getPlatformVersion();
+  }
+
+  Future<void> updateMetadata({required String title, required String artist, required String album}) {
+    return MediaSessionPlatform.instance.updateMetadata(title: title, artist: artist, album: album);
   }
 }
--- lib/media_session_platform_interface.dart
+++ lib/media_session_platform_interface.dart
@@ -3,27 +3,22 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import 'media_session_method_channel.dart';
 
 abstract class MediaSessionPlatform extends PlatformInterface {
-  /// Constructs a MediaSessionPlatform.
   MediaSessionPlatform() : super(token: _token);
 
   static final Object _token = Object();
 
   static MediaSessionPlatform _instance = MethodChannelMediaSession();
 
-  /// The default instance of [MediaSessionPlatform] to use.
-  ///
-  /// Defaults to [MethodChannelMediaSession].
   static MediaSessionPlatform get instance => _instance;
 
-  /// Platform-specific implementations should set this with their own
-  /// platform-specific class that extends [MediaSessionPlatform] when
-  /// they register themselves.
   static set instance(MediaSessionPlatform instance) {
     PlatformInterface.verifyToken(instance, _token);
     _instance = instance;
   }
 
-  Future<String?> getPlatformVersion() {
-    throw UnimplementedError('platformVersion() has not been implemented.');
+  Future<String?> getPlatformVersion();
+
+  Future<void> updateMetadata({required String title, required String artist, required String album}) {
+    throw UnimplementedError('updateMetadata() has not been implemented.');
   }
 }
